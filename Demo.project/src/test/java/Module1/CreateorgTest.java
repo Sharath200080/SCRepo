a251fc3a06c2fbea0a7f41fea502242768cc6cf0
shara <shara@Sharath>
2025-03-17 12:50:18 +0530

    Updated orgTest

--- Demo.project/src/test/java/Module1/CreateorgTest.java
+++ Demo.project/src/test/java/Module1/CreateorgTest.java
@@ -5,6 +5,16 @@ import org.testng.annotations.Test;
 public class CreateorgTest {
 	@Test(groups = "smoketest,Regressiontest")
 	public void orgTest() {
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		String USER = System.getProperty("user");
+		String PASS = System.getProperty("pass");
+		System.out.println(BROWSER);
+		System.out.println(URL);
+		System.out.println(USER);
+		System.out.println(PASS);
+		System.out.println("===========done================");
+		
 		System.out.println("launch browser");
 		System.out.println("login");
 		System.out.println("home page");
@@ -26,13 +36,5 @@ public void orgWithTypeTest() {
 	System.out.println("=============================================");
 	
 }
- @Test
-public void deletorgTest() {
-	System.out.println("launch browser");
-	System.out.println("login");
-	System.out.println("home page");
-	System.out.println("click on new org with type");
-	System.out.println("org type created success");
-	System.out.println("log out ");
-}
+
 }
